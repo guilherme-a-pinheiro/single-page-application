4ad0b0d3a19518e9b70d60be239eec47232412e9
Guilherme Pinheiro <guiapinheiro29@gmail.com>
2022-09-27 22:29:31 -0300

    first commit

---
+++ gulpTasks/servidor.js
@@ -0,0 +1,26 @@
+const gulp = require('gulp')
+const webserver = require('gulp-webserver')
+const watch = require('gulp-watch')
+
+function monitorarArquivos() {
+  return gulp.src('build')
+    .pipe(webserver({
+      port: 8080,
+      open: true,
+      livereload: true,
+    }))
+}
+
+function servidor(cb) {
+  watch('src/**/*.html', () => gulp.series('appHTML')())
+  watch('src/**/*.scss', () => gulp.series('appCSS')())
+  watch('src/**/*.js', () => gulp.series('appJS')())
+  watch('src/assets/imgs/**/*.*', () => gulp.series('appIMG')())
+
+  return cb()
+}
+
+module.exports = {
+  monitorarArquivos,
+  servidor
+}
